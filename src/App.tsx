50d2974d5d80e2cadafe25ffea5503ab248df63d
Man Ping Chen <manpingchen@gmail.com>
2022-03-07 17:17:43 +0000

    declare types for Todos component

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import "./App.css";
 import Todos from "./components/Todos";
 
 function App() {
+  const items = [{ id: 1, title: "Learn React" },{ id: 2, title: "Learn Typescript" }];
   return (
     <div>
-      <Todos />
+      <Todos items={items} />
     </div>
   );
 }
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,8 +1,15 @@
-const Todos = () => {
+import React from "react";
+
+// Use "Generics type"
+// FC stands for Functional Component
+
+type Items = { id: number; title: string }[];
+const Todos: React.FC<{ items: Items }> = (props) => {
   return (
     <ul>
-      <li>Learn React</li>
-      <li>Learn TypeScript</li>
+      {props.items.map((item) => (
+        <li key={item.id}>{item.title}</li>
+      ))}
     </ul>
   );
 };
